acf68280057c0aea5bee27252c5f5c38d0a79445
Alexander Filimonov <filimonov.alexander.s@yandex.ru>
2023-10-07 23:44:12 +0300

    feat: add structure of the projects

---
+++ src/modules/projects/projectsData.tsx
@@ -0,0 +1,30 @@
+export type ProjectsData = {
+  name: string;
+  description: string;
+};
+
+export const projectsKeyboard: ProjectsData = {
+  name: 'Shelter',
+  description: `A simple two-page website with cute pets. Adaptive and responsive layout for mobile and desktop devices.`,
+};
+
+export const projectsMinesweeper: ProjectsData = {
+  name: 'Minesweeper',
+  description: `The project is executed using vanilla JavaScript. Set up the game and play! Light and dark themes are available.`,
+};
+
+export const projectsSelectors: ProjectsData = {
+  name: 'RS Selectors',
+  description: `The project is executed using TypeScript and ES6+ features. Learn the CSS selectors in playing form!`,
+};
+
+export const projectsAsyncRace: ProjectsData = {
+  name: 'Async Race',
+  description: `The SPA is executed using REST architecture with local server interaction. Launch the cars!`,
+};
+
+export const projectsCommerce: ProjectsData = {
+  name: 'e-Commerce',
+  description: `Team work project is executed by 3 members. Project is created using React with functional components, 
+  Ant design component library, Redux and Jest for testing. Store is ready for sales!`,
+};
