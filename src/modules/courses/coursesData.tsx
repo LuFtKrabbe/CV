9f84cf1df2499614f84b38ea7af26fe85732ef82
Alexander Filimonov <filimonov.alexander.s@yandex.ru>
2023-10-07 15:24:17 +0300

    feat: add info about courses

--- src/modules/courses/coursesData.tsx
+++ src/modules/courses/coursesData.tsx
@@ -7,17 +7,17 @@ export type CoursesData = {
 };
 
 export const coursesPreFrontEnd: CoursesData = {
-  name: 'JS/FE Pre-School',
+  name: 'RS School',
   dateStart: '12.2022',
   dateFinish: '02.2023',
-  degree: 'Certificate',
-  description: 'Description',
+  degree: 'JavaScript/Front-end (Pre-School)',
+  description: 'Course for beginners in programming',
 };
 
 export const coursesFrontEnd: CoursesData = {
-  name: 'JavaScript/Front-end',
+  name: 'RS School',
   dateStart: '02.2023',
   dateFinish: '09.2023',
-  degree: 'Certificate',
-  description: 'Description',
+  degree: 'JavaScript/Front-end',
+  description: 'Course for Junior Front-end developer position',
 };
